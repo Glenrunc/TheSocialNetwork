f508a06ecbf0f0b8528f7d1e5dbd9de13e86a5d6
GlenRunc <pourcinematteo@gmail.com>
2024-03-13 12:13:43 +0100

    feature: Try to do something

---
+++ view/navbar.php
@@ -0,0 +1,20 @@
+
+<nav class="navbar navbar-expand-lg bg-success border-bottom border-body">
+  <div class="container-fluid">
+    <!-- add credit page -->
+    <a class="navbar-brand" href="#">
+        <img src="Logo_TZU.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+    </a>
+    <div class="mx-auto d-flex align-items-end">
+      <form class="d-flex" role="search" action="#" method="POST">
+        <input class="form-control me-2 bg-success-subtle" type="search" placeholder="What's up" aria-label="Search">
+        <button class="btn btn-outline-light text-dark  bg-success-subtle" type="submit">Search</button>
+      </form>
+    </div>
+    <div class="d-flex align-items-end">
+        <a href="#">
+            <img src="icons8-person-64.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top" >
+        </a>
+    </div>
+  </div>
+</nav>
