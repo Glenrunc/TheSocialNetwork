a0fbde9d8787fc23dd8e3868c9b58f203e904ea1
RemiTZU <bonnetremi74@gmail.com>
2024-03-18 17:38:58 +0100

    Feat: signup + signin

--- view/signin.php
+++ view/signin.php
@@ -3,67 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In</title>
+    <title>Signin</title>
 </head>
 <body>
 <div class="login-box">
-        <h2>Login</h2>
+        <h2>Signin</h2>
         <form method="POST" action="signin.php">
             <div class="user-box">
-                <input type="text" name="email" required="">
-                <label>E-mail</label>
+                <input type="text" name="email" required="" placeholder="Email">
             </div>
             <div class="user-box">
-                <input type="password" name="password" required="">
-                <label>Password</label>
+                <input type="password" name="password" required="" placeholder="Password">
             </div>
             <input type="submit" id="join-btn" name="join" alt="Join" value="Join">
-
         </form>
 </div>
+<div id = link-login><a href="signup.php">Not register?</a></div>
+
 <?php
-    include 'db_connect.php';
+    require("database.php");
     global $db;
 
     if (!empty($_POST)) {
 
         if (!empty($_POST["password"]) && !empty($_POST["email"])) {
-            $pass = $_POST["password"];
+            $password = $_POST["password"];
             $email = $_POST["email"];
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo ("l'adress n'est pas bonne ");
+                echo "<div class=box-error>Type a valid email</div>";
             }
 
 
-            $query = $db->prepare("SELECT * FROM logins WHERE email = ?");
+            $query = $db->prepare("SELECT * FROM user WHERE email = ?");
             $query->execute([$email]);
             $user = $query->fetch();
             if (!$user) {
-                echo "test";
-                echo ("user or password incorrect");
+                echo "<div class=box-error>email doesn't exist</div>";
             } else {
-
-                if (!password_verify($pass, $user["password"])) {
-                    echo "hey";
-                    echo ("user or password incorrect");
+                if (!password_verify($password, $user["password"])) {
+                    echo "<div class=box-error>password doesn't match</div>";
                 } else {
 
-                    // les verfications sont passées 
-                    // on connecte l'utilisateur
-                    // demarrage d'une session php
-
-
                     session_start();
-                    //stockage des données de l'utilisateur
 
-                    $_SESSION["logins"] = ["email" => $user["email"], "username" => $user["username"], "idlogins" => $user['idlogins'],"adminstate" => $user['adminstate']];
+                    $_SESSION["signin"] = ["email" => $user["email"], "pseudo" => $user["pseudo"]];
 
-                    // on redirige vers une page profil
                     header("Location: index.php");
                 }
             }
         } else {
-            echo ("le formulaire n'est pas complet");
+            echo"<div class=box-error>Fill all the fields</div>";
         }
     }
 
